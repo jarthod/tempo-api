87f7242ef08c673c5c26519201e074071ec670c2
Adrien Rey-Jarthon <jobs@adrienjarthon.com>
2024-02-17 17:24:27 +0100

    use integers instead of strings

--- tempo-api.rb
+++ tempo-api.rb
@@ -3,11 +3,12 @@ require 'zache'
 require 'http'
 
 $cache = Zache.new
+
 # https://www.api-couleur-tempo.fr/api/docs?ui=re_doc#tag/JourTempo
-COLORS = ['inconnu', 'bleu', 'blanc', 'rouge']
+# COLORS = ['inconnu', 'bleu', 'blanc', 'rouge']
 
 get "/" do
   today = $cache.get(:today, lifetime: 600) { HTTP.get("https://www.api-couleur-tempo.fr/api/jourTempo/today").parse['codeJour'] }
   tomorrow = $cache.get(:tomorrow, lifetime: 600) { HTTP.get("https://www.api-couleur-tempo.fr/api/jourTempo/tomorrow").parse['codeJour'] }
-  {today: COLORS[today], tomorrow: COLORS[tomorrow]}.to_json
+  {today: today, tomorrow: tomorrow}.to_json
 end
