88170de6689e1ee73176847c493310f5b755203b
Adrien Rey-Jarthon <jobs@adrienjarthon.com>
2025-02-11 18:01:26 +0100

    Add release step for db migration

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bundle exec puma -p ${PORT} -e ${RACK_ENV:-development}
+release: bundle exec rake db:migrate
--- db/migrate/20250211160613_create_devices.rb
+++ db/migrate/20250211160613_create_devices.rb
@@ -1,9 +1,8 @@
 class CreateDevices < ActiveRecord::Migration[8.0]
   def change
-    # temporary disable as the table is already in production
-    # create_table :devices do |t|
-    #   t.string :mode, null: false, default: 'tempo'
-    #   t.timestamps
-    # end
+    create_table :devices do |t|
+      t.string :mode, null: false, default: 'tempo'
+      t.timestamps
+    end
   end
 end
